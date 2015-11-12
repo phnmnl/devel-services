0dc8d55a5c611376e261d7427eb34052619df75d
Venkata Chandrasekhar Nainala <mailcs76@gmail.com>
2015-11-12 18:39:23 +0000

    Added IPO as submodule

--- IPO/scripts/installIPO.R
+++
@@ -1,7 +0,0 @@
-source("http://bioconductor.org/biocLite.R")
-biocLite("CAMERA")
-biocLite("xcms")
-install.packages("rsm", , repos='http://mirrors.ebi.ac.uk/CRAN') 
-install.packages("devtools",, repos='http://mirrors.ebi.ac.uk/CRAN')
-library("devtools")
-install_github("glibiseller/IPO")
